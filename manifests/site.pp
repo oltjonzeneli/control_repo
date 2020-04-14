efdb1adcd0016f05e770a815aaed959ac9145bb1
root <root@client.puppet.vm>
2020-04-14 10:45:57 -0400

    change

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 node default {
 }
-#node 'master.puppet.vm' {
- #   include role::master_server
-  #  file {'/root/README':
-   #     ensure => file,
-    #    content => "Welcome to ${fqdn}\n",
-    #}
-  #}
+node 'master.puppet.vm' {
+   include role::master_server
+    file {'/etc/secret_password.txt':
+        ensure => file,
+        content => lookup ('secret_password'),
+    }
+  }
 node 'minetest.puppet.vm'  {include role::minecraft_server}
 
 node /^web/ {
