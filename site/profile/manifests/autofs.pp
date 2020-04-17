245227563e363be25f7d305619b065f5b9877e64
oltjonzeneli <63287206+oltjonzeneli@users.noreply.github.com>
2020-04-17 12:37:23 +0200

    Update autofs.pp

--- site/profile/manifests/autofs.pp
+++ site/profile/manifests/autofs.pp
@@ -1,40 +1,18 @@
 class profile::autofs {
   
-  package {'nfs-utils':
-        ensure => installed,
-        allow_virtual => false,
-    }
+ package {'nfs-utils':
+            ensure => installed,
+        }
  
-    service {['nfs','rpcbind']:
-        ensure => running,
-        require => Package['nfs-utils'],
-    }
- 
-    exec {'/usr/sbin/exportfs -ra':
-        refreshonly => true
-    }
- 
-    file {'/var/share':
-        ensure => directory,
-    }
- 
-    file {'/var/readonly':
-        ensure => directory,
-    }
- 
-    file_line {'export share':
-        path => '/etc/exports',
-        line => '/var/share -rw,sync,no_root_squash 10.0.0.0/8',
-        match => '^/var/share',
-        require => Service['nfs','rpcbind'],
-        notify => Exec['/usr/sbin/exportfs -ra'],
-    }
- 
-    file_line {'export readonly':
-        path => '/etc/exports',
-        line => '/var/readonly -ro,sync,no_root_squash 10.0.0.0/8',
-        match => '^/var/readonly',
-        require => Service['nfs','rpcbind'],
-        notify => Exec['/usr/sbin/exportfs -ra'],
+        file { '/mnt/nfs:
+            ensure => directory,
+        }
+        mount { '/mnt/nfs':
+            ensure => mounted,
+            atboot => true,
+            device => "192.168.32.182:/var/nfsshare",
+            fstype => 'nfs',
+            options => 'rw',
+            require => [File['/nfs/readonly'],Mount['/nfs/share']]
     }
 }
