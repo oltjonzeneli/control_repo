9697bae88b4a34f8fb6697522dc42dadb767a2fc
oltjonzeneli <63287206+oltjonzeneli@users.noreply.github.com>
2020-05-22 16:20:41 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node {'web.puppet.vm':}
-  dockeragent::node {'db.puppet.vm':}
+  dockeragent::node {'web1.puppet.vm':}
+  dockeragent::node {'db1.puppet.vm':}
 }
