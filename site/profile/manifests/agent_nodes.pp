24a79e9d7d97cea0cdd96f613e610681ac149c34
oltjonzeneli <root@master.puppet.vm>
2020-05-27 23:10:26 +0200

    aafsm

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,15 +1,19 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node {'web.puppet.vm':}
-  dockeragent::node {'db.puppet.vm':}
-  host {'web.puppet.vm':
-    ensure => present,
-    ip     =>'172.18.0.2',
-    }
-  host {'db.puppet.vm':
-    ensure => present,
-    ip     => '172.18.0.3',
-    }
-  dockeragent::node {'web1.puppet.vm':}
-  dockeragent::node {'db1.puppet.vm':}
-}
+ 	 dockeragent::node {'web.puppet.vm':}
+         dockeragent::node {'web1.puppet.vm':}
+ 	 dockeragent::node {'db.puppet.vm':}
+       
+      host {'web.puppet.vm':
+            ensure => present,
+            ip => '172.18.0.2',
+            }
+      host {'web1.puppet.vm':
+            ensure => present,
+            ip => '172.18.0.4',
+            }
+      host {'db.puppet.vm':
+            ensure => present,
+            ip => '172.18.0.3',
+            }     
+     }
