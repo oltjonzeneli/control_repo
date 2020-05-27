8d7597285ec25b355dbaff81271d0f7cbfc83809
oltjonzeneli <root@master.puppet.vm>
2020-05-27 22:24:06 +0200

    fdn

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,10 +1,6 @@
 class profile::base {
   user {'admin':
     ensure => present,
-<<<<<<< HEAD
   }
   include profile::ssh_server
-=======
-    }
->>>>>>> a65d8544fb597be7118caa8abb85db12637699f9
 }
