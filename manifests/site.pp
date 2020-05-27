5372a1886200e516596a67b9e824844e7e8338b8
oltjonzeneli <root@master.puppet.vm>
2020-05-27 22:15:59 +0200

    bcns

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,37 +1,9 @@
-node default {
-}
-node 'master2.puppet.vm' {
-   include role::master_server
-    file {'/etc/secret_password.txt':
-        ensure => file,
-        content => lookup ('secret_password'),
-    }
-  }
-node 'minetest.puppet.vm'  {include role::minecraft_server}
-
-node /^web/ {
-    include role::app_server
- }
- 
- node /^db/ {
-    include role::db_server
- }
-
 node master.puppet.vm {
    include role::master_server
    file {'/etc/secret_password.txt':
 	ensure => file,
 	content => lookup ('secret_password')
 	}	
-}
-  file { '/root/README':
-    ensure => file,
-    content => 'This is a Readme',
-    owner => 'root',
-  }
-  
-  notify { 'This is a production environment':
-  }
 }
 
 node /^web/                                                 { include role::app_server }
