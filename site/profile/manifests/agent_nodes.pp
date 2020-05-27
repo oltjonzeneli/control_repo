5066cb5e326142f5249f4263159ff9b5bd32dcfb
oltjonzeneli <root@master.puppet.vm>
2020-05-27 22:25:41 +0200

    fhn

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,6 +1,5 @@
 class profile::agent_nodes {
   include dockeragent
-<<<<<<< HEAD
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
   dockeragent::node {'minetest.puppet.vm':}
@@ -12,8 +11,6 @@ class profile::agent_nodes {
     ensure => present,
     ip     => '172.18.0.3',
     }
-=======
   dockeragent::node {'web1.puppet.vm':}
   dockeragent::node {'db1.puppet.vm':}
->>>>>>> a65d8544fb597be7118caa8abb85db12637699f9
 }
