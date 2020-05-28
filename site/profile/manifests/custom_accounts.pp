1a18dcc3c46a5fa316b189914184c912d010a9a6
oltjonzeneli <root@master.puppet.vm>
2020-05-28 05:39:47 +0200

    pass hash

--- site/profile/manifests/custom_accounts.pp
+++ site/profile/manifests/custom_accounts.pp
@@ -22,7 +22,7 @@ class profile::custom_accounts {
           group    => 'phase3',
           shell    => '/bin/bash',
           comment  => 'Test account',
-	  password => '!phase3!',
+	  password => '$6$6CYldZC3$ht6Yv8M8PqWpNwcpZuRRCKEvqnQ7cbhDXQbtWtUaeaMvDBvXIoFEuI.5WJ9Dy6qROQHmrNI2CDV6u7TSybzdT/',
           locked   => false,
         }
 	
